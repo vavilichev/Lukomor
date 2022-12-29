50aac7ab8ded6edfa0f7ce753e4231ef3e938b6e
vavilichev <andreyvavilichev@gmail.com>
2022-12-29 16:09:02 +0400

    UPD v.4.0.3: Sintax fix

--- Lukomor/Scripts/Presentation/UI/Views/Windows/IWindowOpenHandler.cs
+++ Lukomor/Scripts/Presentation/UI/Views/Windows/IWindowOpenHandler.cs
@@ -5,7 +5,7 @@ namespace Lukomor.Presentation.Views.Windows
     public interface IWindowOpenHandler
     {
         WindowViewModel OpeningWindowViewModel { get; }
-        IWindow OpeningWindow { get; };
+        IWindow OpeningWindow { get; }
         IWindowOpenHandler AddPayloads(params Payload[] payloads);
         void WithBackDestination<TWindowViewModel>() where TWindowViewModel : WindowViewModel;
     }
