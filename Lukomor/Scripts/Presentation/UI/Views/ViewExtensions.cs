fdc105bee55a4e0a6288f92077b62209781f98f2
vavilichev <andreyvavilichev@gmail.com>
2023-01-09 21:46:02 +0400

    UPD v.4.0.13 Added extension for casting view to any IView realization class. Use view.As<YourRealization>();

---
+++ Lukomor/Scripts/Presentation/UI/Views/ViewExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Lukomor.Presentation.Views
+{
+    public static class ViewExtensions
+    {
+        public static T As<T>(this IView view) where T : IView
+        {
+            return (T) view;
+        }
+    }
+}
