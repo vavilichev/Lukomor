e09411a48090d68c9e1d71864009c86365da59d5
Andrei <av@madeonearth.games>
2026-01-31 15:00:04 +0100

    Cleaning iteration, ObservableCollectionBinder is fully completed

--- Lukomor/Scripts/MVVM/Binders/BinderBase.cs
+++ Lukomor/Scripts/MVVM/Binders/BinderBase.cs
@@ -23,11 +23,28 @@ namespace Lukomor.MVVM.Binders
         }
 
 #if UNITY_EDITOR
+        /// <summary>
+        /// This method uses by MVVMValidator to understand, is it necessary
+        /// to draw warning exclamation mark nearby the GameObject in the hierarchy or not.
+        /// Different type of binders may have different approaches to understand is it broken or not
+        /// </summary>
+        /// <returns></returns>
         public abstract bool IsBroken();
 
+        /// <summary>
+        /// This method uses in cases when someone "above" gets the broken links, and it's affected
+        /// the current binder. Each binder type may handle this method different.
+        /// </summary>
         public abstract void SmartReset();
 
-        protected bool IsBrokenBasic(string propertyName, out Type sourceViewModelType)
+        /// <summary>
+        /// This method just checks the Source View and it's ViewModel. If Source View is absent or
+        /// it's ViewModel is invalid this method returns true
+        /// </summary>
+        /// <param name="sourceViewModelPropertyName"></param>
+        /// <param name="sourceViewModelType"></param>
+        /// <returns></returns>
+        protected bool IsBrokenBasic(string sourceViewModelPropertyName, out Type sourceViewModelType)
         {
             sourceViewModelType = null;
             
@@ -36,7 +53,7 @@ namespace Lukomor.MVVM.Binders
                 return true;    // source view must be selected
             }
             
-            if (string.IsNullOrEmpty(propertyName))
+            if (string.IsNullOrEmpty(sourceViewModelPropertyName))
             {
                 return true;    // command property must be selected
             }
